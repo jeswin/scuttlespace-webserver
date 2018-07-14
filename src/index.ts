dcf1a0604c1be842ef0b66361bffec20915eba38
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-14 17:08:34 +0530

    Initial commits

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import Koa = require("koa");
 import bodyParser = require("koa-bodyparser");
+import router = require("koa-route");
+import * as site from "./site";
+
 const app = new Koa();
 app.use(bodyParser());
 
-app.use(async ctx => {
-  const { username, mod, template } = await hosting.extractDestination(
-    ctx.hostname,
-    ctx.path
-  );
-
-  ctx.username = username;
-  ctx.mod = mod;
-
-  ctx.body = "Hello World";
-});
+[
+  router.get("/", site.home)
+  // router.get("/:username", user.home),
+  // router.get("/:username/posts", pub.home),
+  // router.get("/:username/posts/category/:category", pub.category),
+  // router.get("/:username/posts/:post", pub.item),
+  // router.get("/:username/resume", resume.home),
+  // router.get("/:username/projects", projects.home),
+  // router.get("/:username/bookmarks", bookmarks.home),
+  // router.get("/:username/bookmarks/category/:category", bookmarks.category)
+].forEach(r => app.use(r));
 
 app.listen(3000);
+console.log("Listening on 3000");
