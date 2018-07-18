ad7706aeccf11f3fc6be0959a8e72a36a38d8cfa
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-18 13:13:43 +0530

    WIP

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,6 +1,6 @@
 import { Context } from "koa";
-import * as feedService from "scuttlespace-service-feed";
 import * as cmsService from "scuttlespace-service-cms";
+import * as feedService from "scuttlespace-service-feed";
 import * as templates from "../templates";
 
 export async function home(ctx: Context, username: string) {
@@ -8,8 +8,8 @@ export async function home(ctx: Context, username: string) {
   const template = await templates.getTemplate(templateName);
   
   const feed = await feedService.getFeed(username);
-  const blogroll = await cmsService.getContent("blogroll");
   const dateLinks = await feedService.getDateLinks(username);
+  const blogroll = await cmsService.getContent("blogroll");
   const pinned = await cmsService.getContent("pinned");
   
   ctx.body = await template.user.home({ blogroll, dateLinks, feed, pinned });  
